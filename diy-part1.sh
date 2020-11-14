2ba0736321f2c6cb777ed98d01c1c3505fc06766
acdxxx177 <ye1991xin@acdxxx177.win>
2020-11-14 23:10:32 +0800

    取消新加的源

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#增加新源
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-git clone https://github.com/jefferymvp/luci-app-koolproxyR ./package/luci-app-koolproxyR
