78c52dd982863287b27af21cb282542b30207b97
acdxxx177 <ye1991xin@163.com>
2022-10-25 20:48:34 +0800

    分开编译

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,11 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#vlmcsd
+git clone https://github.com/cokebar/openwrt-vlmcsd.git ./package/openwrt-vlmcsd
+git clone https://github.com/cokebar/luci-app-vlmcsd.git ./package/luci-app-vlmcsd
+
+#xray
+git clone https://github.com/yichya/openwrt-xray.git ./package/openwrt-xray
+git clone https://github.com/yichya/luci-app-xray.git ./package/luci-app-xray
--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,16 @@
 #
 
 # 更改路由IP地址
-sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+# 替换系统名字
+sed -i "s/hostname='OpenWrt'/hostname='Aacxxx177_OpenWrt'/g" package/base-files/files/bin/config_generate
+# 替换时区
+sed -i "s/timezone='UTC'/timezone='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+# 替换ntp服务器
+sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp2.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/ntp3.aliyun.com/g' package/base-files/files/bin/config_generate
 
 # 更改DHCP的传参方式,用于IPTV配置
 sed -i 's/${vendorid:+-V "$vendorid"}/-V ""/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
